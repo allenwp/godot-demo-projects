f8ed372ea117e660511d3507041b22194e88c959
Allen Pestaluky <allenpestaluky@gmail.com>
2026-04-17 16:21:51 -0400

    Improved viewport scaling

--- misc/hdr_output/tonemap/subviewport_scale.gd
+++ misc/hdr_output/tonemap/subviewport_scale.gd
@@ -1,16 +1,20 @@
 extends Node
 
-@onready var viewport: SubViewport = $SubViewport
-@onready var viewport_initial_size: Vector2i = viewport.size
+@export var sub_viewport_initial_size: Vector2
+@export var main_viewport_initial_size: Vector2
+@onready var sub_viewport: SubViewport = $SubViewport
 @onready var viewport_sprite: Sprite2D = $ViewportSprite
 
 
 func _ready() -> void:
 	get_viewport().size_changed.connect(_root_viewport_size_changed)
+	_root_viewport_size_changed()
 
 
 func _root_viewport_size_changed() -> void:
-	# Automatically change viewport resolution according to the window size.
-	# This ensures the viewport remains crisp at window sizes higher than the default.
-	viewport.size = Vector2.ONE * get_viewport().size.y
-	viewport_sprite.scale = Vector2.ONE * viewport_initial_size.y / get_viewport().size.y
+	# Automatically change sub_viewport resolution according to the window size.
+	# This ensures the sub_viewport remains crisp at window sizes higher than the default.
+	sub_viewport.size.x = sub_viewport_initial_size.x * (get_viewport().size.y / main_viewport_initial_size.y)
+	sub_viewport.size.y = sub_viewport_initial_size.y * (get_viewport().size.y / main_viewport_initial_size.y)
+	viewport_sprite.scale.x = sub_viewport_initial_size.x / sub_viewport.size.x
+	viewport_sprite.scale.y = sub_viewport_initial_size.y / sub_viewport.size.y
